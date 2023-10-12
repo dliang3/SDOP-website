5046e2024b6378a0dee96ae09d09c3d856a97ede
Daniela Liang <artaffu@gmail.com>
2023-10-12 17:11:49 -0400

    extra changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home/Home';
 import Guide from './pages/Guide/Guide';
 import Download from './pages/Download/Download';
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,15 @@
 import "./Home.css"
 
 const Home = () => {
+
+  const handleOnClick = () => {
+    
+  }
+
   return (
     <div>
       <h1>Silly Data Object Packager</h1>
+      <button onClick={handleOnClick}>Download Here</button>
     </div>
   );
 }
