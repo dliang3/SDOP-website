cce8e46a3acdae3e151816353c354505e30647ad
Daniela Liang <artaffu@gmail.com>
2023-10-30 15:21:51 -0400

    footer

--- src/pages/Guide/Guide.js
+++ src/pages/Guide/Guide.js
@@ -2,8 +2,11 @@ import "./Guide.css"
 
 const Guide = () => {
   return (
-    <div>
+    <div className="guide">
       <h1>Getting Started</h1>
+      <body className="guideBody">Here we will provide the code library you can access to code with the files after packaging them. <br></br> 
+      There will also be a tutorial for how to download and tutorial for using the app.
+      </body>
     </div>
   );
 }
