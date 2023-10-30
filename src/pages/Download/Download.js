c9b0e1a5dd65b91675bc97560f963c43d014765f
Daniela Liang <artaffu@gmail.com>
2023-10-30 13:54:19 -0400

    test mareep download

--- src/pages/Download/Download.js
+++ src/pages/Download/Download.js
@@ -6,9 +6,14 @@ const Download = () => {
       <h1>Download</h1>
       <div className="SystemDownloadBlock">
         <h2>Windows</h2>
-        <button className="mainButton">
-          Download Version 1.0
-        </button>
+        <h3>64-bit</h3>
+        {/* <a href="../../appCode/SDOP_app.zip" download="SDOP_app.zip"> */}
+        <a href={require("../../appCode/test_mareep.png")} download="test_mareep">
+          <button className="mainButton">
+            Download Version 1.0
+          </button>
+        </a>
+
         <h2>Mac</h2>
         <button className="mainButton">
           Download Version 1.0
