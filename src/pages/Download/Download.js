17853301bdf09fd1140238ae02cb5610a493668e
Daniela Liang <artaffu@gmail.com>
2023-10-30 13:37:16 -0400

    added styles to download

--- src/pages/Download/Download.js
+++ src/pages/Download/Download.js
@@ -4,6 +4,20 @@ const Download = () => {
   return (
     <div>
       <h1>Download</h1>
+      <div className="SystemDownloadBlock">
+        <h2>Windows</h2>
+        <button className="mainButton">
+          Download Version 1.0
+        </button>
+        <h2>Mac</h2>
+        <button className="mainButton">
+          Download Version 1.0
+        </button>
+        <h2>Linux</h2>
+        <button className="mainButton">
+          Download Version 1.0
+        </button>
+      </div>
     </div>
   );
 }
