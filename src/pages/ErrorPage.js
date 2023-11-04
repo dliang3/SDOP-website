3936bb31543b49e0adc8da657a5dfa59e4d85cd5
Daniela Liang <artaffu@gmail.com>
2023-11-03 20:49:23 -0400

    ico, name, eror page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom"
 
 const NotFound = () => {
   return (
-    <div className="not-found">
-      <h2>Sorry</h2>
-      <p>That page cannot be found</p>
+    <div style={{ height: '1000px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <h2 style={{ marginBottom: '20px'}}>Sorry </h2>
+      <p>That page cannot be found. </p>
       <Link to="/">Back to Home.</Link>
     </div>
   );
