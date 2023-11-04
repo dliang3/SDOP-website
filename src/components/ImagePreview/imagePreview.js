203d643202df24e21ea66c3f194f169f546e421c
Daniela Liang <artaffu@gmail.com>
2023-11-03 22:12:59 -0400

    image previews

---
+++ src/components/ImagePreview/imagePreview.js
@@ -0,0 +1,14 @@
+import './imagePreview.css'; 
+
+const ImagePreview = ({handleCloseImage, handleImageClick, selectedImage}) => {
+  
+  return (
+    <div className="imagePreviewOverlay" onClick={handleCloseImage}>
+      <div className="imagePreview" onClick={handleImageClick}>
+        <img src={selectedImage} alt="preview" />
+      </div>
+    </div>
+  );
+};
+
+export default ImagePreview;
--- src/pages/Download/Download.js
+++ src/pages/Download/Download.js
@@ -1,10 +1,25 @@
+import React, { useState } from 'react';
+import ImagePreview from '../../components/ImagePreview/imagePreview.js';
 import "./Download.css"
 
 const Download = () => {
+  // logic for handling image preview
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageClick = (imageSrc) => {
+    setSelectedImage(imageSrc);
+  };
+
+  const handleCloseImage = () => {
+    setSelectedImage(null);
+  };
+
   return (
     <div className="download">
       <h1>Download</h1>
       <div className="SystemDownloadBlock">
+
+        {/* Download links */}
         <h2>Windows</h2>
         <h3>64-bit</h3>
         <a href={require("../../test/SDOP.msi")} alt="SDOP app">
@@ -12,7 +27,6 @@ const Download = () => {
             Download Version 1.0
           </button>
         </a>
-
         <h2>Mac</h2>
         <button className="mainButton">
           Download Version 1.0
@@ -23,32 +37,45 @@ const Download = () => {
         </button>
       </div>
 
+      {/* Download Guide */}
       <div className="downloadGuide">
         <div className="downloadTextBlock">
           <h2 className="line">Download Guide</h2>
-          <div className="line"> 1. Choose the corresponding download for your operating system. <br />
-            2. Once the installer is downloaded, open it and follow the instructions for setting up the app. <br/>
-            Note: Remember where you installed the app so that you can create a shortcut for it! <br/>
+          <div className="line"> 1. Choose the corresponding download for your operating system. <br /> </div>
+          <div className="line"> 2. Once the installer is downloaded, open it and follow the instructions for setting up the app. <br /> </div>
+          <div className="line"> Note: Remember where you installed the app so that you can create a shortcut for it! <br /> </div>
 
-          </div>
+          <div className="imageLayout">
 
-          <div className="image_layout">
-            <img src={require("../../images/download/setup_wizard.png")} alt="setup wizard" />
+            <img src={require("../../images/download/setup_wizard.png")} alt="setup wizard" onClick={() => handleImageClick(require("../../images/download/setup_wizard.png"))} />
             <div className="arrow" />
-            <img src={require("../../images/download/setup2.png")} alt="setup 2" />
+            <img src={require("../../images/download/setup2.png")} alt="setup 2" onClick={() => handleImageClick(require("../../images/download/setup2.png"))} />
             <div className="arrow" />
-            <img src={require("../../images/download/setup3.png")} alt="setup 3" />
+            <img src={require("../../images/download/setup3.png")} alt="setup 3" onClick={() => handleImageClick(require("../../images/download/setup3.png"))} />
             <div className="arrow" />
-            <img src={require("../../images/download/setup4.png")} alt="setup 4" />
+            <img src={require("../../images/download/setup4.png")} alt="setup 4" onClick={() => handleImageClick(require("../../images/download/setup4.png"))} />
           </div>
 
           <div className="line">
             3. To create a shortcut, go to the directory where SDOP was downloaded, right click on main.exe, and create a shortcut.
           </div>
+
+          <img src={require("../../images/download/create_shortcut.png")} alt="shortcut" onClick={() => handleImageClick(require("../../images/download/create_shortcut.png"))} />
+
+          <div className="line">
+            4. Double-click your shortcut or main.exe to run the application.
+          </div>
         </div>
-
-
       </div>
+
+      {/* Handle enlarged image preview */}
+      {selectedImage && (
+        <ImagePreview
+          handleCloseImage={handleCloseImage}
+          handleImageClick={handleImageClick}
+          selectedImage={selectedImage}
+        />
+      )}
     </div>
   );
 }
