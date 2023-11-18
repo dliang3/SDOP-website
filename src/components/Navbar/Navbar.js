6710363fdc883c4284ee6ce162518e526e2973d8
Daniela <artaffu@gmail.com>
2023-11-17 20:07:08 -0500

    extra test

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,17 +5,17 @@ import logo from "../../images/SDOP-logo.png"
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <Link to="/">
+      <Link to="/SDOP-website">
         <h1>Silly Data <br></br> Object Packager</h1>
       </Link>
-      <Link to="/">
+      <Link to="/SDOP-website">
         <div className="logo">
           <img src={logo} alt="SDOP-Logo"></img>
         </div>
       </Link>
 
       <div className="links">
-        <Link to="/" className="hoverBorder">Home</Link>
+        <Link to="/SDOP-website" className="hoverBorder">Home</Link>
         <Link to="/Guide" className="hoverBorder">Guide</Link>
         <Link to="/Download" className="hoverBorder">Download</Link>
         <Link to="/FAQ" className="hoverBorder">FAQ</Link>
